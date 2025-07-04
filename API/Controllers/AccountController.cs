578ebd6863adfbd8b5c17f6b4e6f432ece7089b1
Bernardo Barata <bbarata@globalblue.com>
2025-07-04 16:50:08 +0100

    feat: add auth init

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController(AppDbContext context) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if (await EmailExists(registerDto.Email))
+            {
+                return BadRequest("Email taken");
+            }
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        private async Task<bool> EmailExists(string email)
+        {
+            return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+        }
+    }
+}
